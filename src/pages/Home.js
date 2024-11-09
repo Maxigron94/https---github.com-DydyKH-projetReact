9bcef4228119675696441f224fe0f8d60a211ae9
Maxime Grondin <Maxigron94@hotmail.com>
2024-11-09 13:22:37 -0500

    ajout home.css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
 
 function Home() {
     return (
-        <div>
+        <div className="home">
+            <div className="headerContainer">
+
+            </div>
 
         </div>
     )
