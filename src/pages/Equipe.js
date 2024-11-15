f73a0206dca2307db237d93a229e8a1484034357
Dydy <altis.audy@gmail.com>
2024-11-15 18:44:38 -0500

    Page équipe fini !

--- src/pages/Equipe.js
+++ src/pages/Equipe.js
@@ -1,11 +1,50 @@
 import React from 'react'
+import audy from "../assets/a.jpg";
+import felix from "../assets/f.jpeg";
+import maxime from "../assets/m.jpeg";
 
 function Equipe() {
     return (
-        <div>
-
-            page Équipe
-
+        <div id='equipe'>
+            <h1>Une équipe qui va vous exposer tout les faits !</h1>
+            <div>
+                <div className='container'>
+                    <img className="avatar" src={audy} alt="Himiko Toga"/>
+                    <div className='overlay'>
+                        <div className="texte">
+                            <h2>Audy Altis</h2>
+                            <ul>
+                                <li>Développeur graphique</li>
+                                <li>La personne qui a fait le designe enfin plus ou moins</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className='container'>
+                    <img className="avatar" src={felix} alt="Evelynn"/>
+                    <div className='overlay'>
+                        <div className="texte">
+                            <h2>Felix Laventure</h2>
+                            <ul>
+                                <li>Développeur PROfessionel</li>
+                                <li>La personne qui a souffert pour lié la bd avec React</li>                                
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className='container'>
+                    <img className="avatar" src={maxime} alt="Singe penseur"/>
+                    <div className='overlay'>
+                        <div className="texte">
+                            <h2>Maxime Grondin</h2>
+                            <ul>
+                                <li>Chef de projet</li>
+                                <li>La personne qui dirige le projet ???</li>                                
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
