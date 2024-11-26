3fce35d152db9471da1a5cced7fb252fc7ba8c04
Dydy <altis.audy@gmail.com>
2024-11-25 20:41:39 -0500

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import RandomFilm from '../components/RandomFilm';
 import BannerImage from "../assets/back2.jpg";
-import ExterImage from "../assets/exterieur.png";
 import "../styles/Home.css";
 
 
 const Home = () => {
     return (
         
-        <div className="home" style={{ backgroundImage: `url(${ExterImage})`}}>
+        <div className="home">
             <div className="homeContainer" style={{ backgroundImage: `url(${BannerImage})` }}>
                 <RandomFilm />
             </div>
